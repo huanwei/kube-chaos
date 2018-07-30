a1497c19e83d4e076c0c1c4a614030672a82c25d
BForever <11fhc@163.com>
2018-07-30 16:12:27 +0800

    interface name got

---
+++ testpod/autobuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t kube-chaos:v0.2 .
---
+++ testpod/autodeploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl delete ds chaos -n kube-system
+kubectl delete deployment test
+kubectl apply -f testpod.yaml
+kubectl apply -f ../chaos-daemonset.yaml
+
